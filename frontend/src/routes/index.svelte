1a37183f43b37c5b4090827eed23383f79806a30
diamondburned <diamond@arikawa-hi.me>
2022-04-15 17:33:24 -0700

    Add miniCSS

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,36 +1,31 @@
-<!-- <script>
-</script>
-
-<<<<<<< Updated upstream
 <main class="background">
-	<div class="loggingIn">Logging in</div>
+	<div class=""
+	<div class="spinner" />
+	<h3 class="loggingIn">Logging in</h3>
 </main>
 
 <style>
-	.loggingIn {
-=======
-<div class="background">
-	<div class="loggingIn">Logging in</div>
-</div>
-
-<style>
-	.background {
-		box-sizing: border-box;
-		background: linear-gradient(
-			to bottom,
-			rgba(247, 168, 184, 1) 0%,
-			rgba(234, 171, 217, 1) 25%,
-			rgba(200, 181, 245, 1) 50%,
-			rgba(147, 194, 255, 1) 75%,
-			rgba(85, 205, 252, 1) 100%
-		);
-		height: 100vh;
-	}
-
-	.loggingIn {
+	main {
 		display: flex;
-		justify-content: center;
+		flex-direction: column;
 		align-items: center;
->>>>>>> Stashed changes
+		justify-content: center;
+
+		padding: 12px;
+		background-color: rgba(255, 255, 255, 0.25);
 	}
-</style> -->
+
+	.spinner {
+		--spinner-fore-color: var(--accent-foreground);
+		--spinner-back-color: rgba(255, 255, 255, 0.25);
+
+		width: 64px;
+		height: 64px;
+		border-width: 8px;
+	}
+
+	.loggingIn {
+		margin-top: 1em;
+		color: var(--accent-foreground);
+	}
+</style>
